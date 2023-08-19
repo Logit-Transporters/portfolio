a0d52cd9ebb9fdf6d94d8b676dd8bd3c0c09409e
michaelgobz <raniahgobz@outlook.com>
2023-08-19 10:42:28 +0000

    added the mvp services

--- components/Features/featuresData.tsx
+++ components/Features/featuresData.tsx
@@ -3,12 +3,11 @@ import { Feature } from "@/types/feature";
 const featuresData: Feature[] = [
   {
     id: 1,
-    icon: "/images/icon/icon-17.svg",
-    title: "Package Delivery",
-    description:
-      "Package delivery is service that enables you move products from your location (or transportation hub)" +
-      " to the end user’s location. We offer a variety of last mile delivery services to meet " +
-      "your needs to get your product to your customer. This includes storage or warehousing upto 5 days",
+    icon: "/images/icon/icon-22.svg",
+    title: "Business Operations Outsourcing",
+    description:"Business process outsourcing (BPO) is the contracting of non-primary business activities and functions to a third-party provider. BPO services include payroll, human resources (HR), accounting and customer/call center relations. BPO is also known as information technology enabled services (ITS)."+
+    "We offer a variety of BPO services to meet your needs to get your product to your customer. This includes storage or warehousing upto 5 days"
+      ,
   },
   {
     id: 2,
@@ -21,46 +20,12 @@ const featuresData: Feature[] = [
   },
   {
     id: 3,
-    icon: "/images/icon/icon-11.svg",
+    icon: "/images/icon/icon-21.svg",
     title: "Fulfillment",
     description:
       "Fulfillment is the process of receiving, packaging and shipping orders for goods. " +
       "We offer a variety of fulfillment services to meet your needs to get your product to your customer. " +
       "This includes storage or warehousing upto 5 days",
-  },
-  {
-    id: 4,
-    icon: "/images/icon/icon-07.svg",
-    title: "Freight Services",
-    description:
-      "Freight Services is a leading provider of integrated logistics solutions in the Middle East. " +
-      "We deliver cargo from anywhere in the world to anywhere in the world. " +
-      "Our air, ocean, land and multi-modal freight forwarding services ensure that your cargo reaches " +
-      "its destination on time and on budget." +
-      "We offer a full range of freight forwarding services including air freight, ocean freight, road freight, " +
-      "cross-border services, customs clearance, warehousing and distribution, project cargo, logistics and supply chain management. ",
-  },
-  {
-    id: 5,
-    icon: "/images/icon/icon-09.svg",
-    title: "Cargo Clearance",
-    description:
-      "Cargo Clearance is a service  where we offer cargo clearance for our customers from ports along the " +
-      "East African cost and then Transport to our customers premises or to our warehousing through the services" +
-      "This service is offered to our customers who have their own cargo and need to clear it from the port and transport it to their premises or to our warehousing. " +
-      "We may require some more specific information from you to ensure that we can provide you with the best possible service. " +
-      "Please contact us for more information. through the portal control panel. ",
-  },
-  {
-    id: 6,
-    icon: "/images/icon/icon-20.svg",
-    title: "WareHouse Management",
-    description:
-      "Warehousing is a service where we offer storage for our customers cargo in our warehouses. " + "" +
-      "This service is offered to our customers who have their own cargo and need to store it in our warehouses. " +
-      "We may require some more specific information from you to ensure that we can provide you with the best possible service. " +
-      " this provides in Tiers Tier 1, Tier 2 and Tier 3. " +
-      "But by default we offer Tier 1 warehousing. of 5days of storage before we can return it to the concerned parties ",
   },
 ];
 
