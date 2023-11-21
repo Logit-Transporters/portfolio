6c6f79fc0b7bfbfaffe0a31c7cf05d86ea617d6c
michaelgobz <raniahgobz@outlook.com>
2023-11-21 17:14:43 +0300

    features updated

--- components/Features/featuresData.tsx
+++ components/Features/featuresData.tsx
@@ -4,9 +4,9 @@ const featuresData: Feature[] = [
   {
     id: 1,
     icon: "/images/icon/icon-22.svg",
-    title: "Business Operations Outsourcing",
-    description:"Business process outsourcing (BPO) is the contracting of non-primary business activities and functions to a third-party provider. BPO services include payroll, human resources (HR), accounting and customer/call center relations. BPO is also known as information technology enabled services (ITS)."+
-    "We offer a variety of BPO services to meet your needs to get your product to your customer. This includes storage or warehousing upto 5 days"
+    title: "BPO",
+    description:", human resources (HR), accounting and customer/call center relations. BPO is also known as information technology enabled services (ITS)."+
+    "We offer a variety of BPO services to meet your needs to get your product to your customer."
       ,
   },
   {
@@ -14,16 +14,14 @@ const featuresData: Feature[] = [
     icon: "/images/icon/icon-15.svg",
     title: "Last Mile Delivery",
     description:
-      "Last mile delivery is the last step in moving your business’ product from your location (or transportation hub)" +
-      " to the end user’s location. We offer a variety of last mile delivery services to meet " +
-      "your needs to get your product to your customer. This does not include storage or warehousing.",
+      " We offer a variety of last mile delivery services to meet " +
+      "your needs to get your product to your customer.",
   },
   {
     id: 3,
     icon: "/images/icon/icon-21.svg",
     title: "Fulfillment",
     description:
-      "Fulfillment is the process of receiving, packaging and shipping orders for goods. " +
       "We offer a variety of fulfillment services to meet your needs to get your product to your customer. " +
       "This includes storage or warehousing upto 5 days",
   },
