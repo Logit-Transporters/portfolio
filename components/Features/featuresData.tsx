03b03c392cf1935ac14b4c070f4a93afd40361e6
michaelgobz <raniahgobz@outlook.com>
2023-11-21 17:23:58 +0300

    features updated

--- components/Features/featuresData.tsx
+++ components/Features/featuresData.tsx
@@ -5,7 +5,7 @@ const featuresData: Feature[] = [
     id: 1,
     icon: "/images/icon/icon-22.svg",
     title: "BPO",
-    description:", human resources (HR), accounting and customer/call center relations. BPO is also known as information technology enabled services (ITS)."+
+    description:" BPO is also known as information technology enabled services (ITS)."+
     "We offer a variety of BPO services to meet your needs to get your product to your customer."
       ,
   },
@@ -22,9 +22,9 @@ const featuresData: Feature[] = [
     icon: "/images/icon/icon-21.svg",
     title: "Fulfillment",
     description:
-      "We offer a variety of fulfillment services to meet your needs to get your product to your customer. " +
-      "This includes storage or warehousing upto 5 days",
-  },
+      "We offer a variety of fulfillment services to meet your needs to get your product to your customer.",
+  }
+
 ];
 
 export default featuresData;
